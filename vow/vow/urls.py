898b4dd8df48a1ce76077bc5e3c5924c659c3f6b
Conceal016 <1804508631@qq.com>
2024-11-03 01:21:38 +0800

    '注册页面'

--- vow/vow/urls.py
+++ vow/vow/urls.py
@@ -13,9 +13,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.etree.ElementInclude import include
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+
+#1.导入系统的logging
+import logging
+# #2.创建（获取）日志器
+# logger=logging.getLogger('django')
+#
+# from django.http import HttpResponse
+# def log(request):
+#     #3.使用日志器记录信息
+#     logger.info('info')
+#     return HttpResponse('test')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('',log),
+    # include 参数1要设置为元组（urlconf_module, app_name）
+    # urlconf_module 子应用的路由
+    # app_name 子应用的名字
+    # namespace 设置命名空间
+    path('', include(('users.urls', 'users'), namespace='users')),
 ]
+
